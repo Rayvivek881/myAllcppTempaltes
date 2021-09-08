6003411892863e5806c6a108094f4e2ba50ea0c0
vivek kumar ray <56621398+Rayvivek881@users.noreply.github.com>
2021-09-07 19:39:15 -0700

    Update suffixArray.cpp

--- templates/strings/suffixArray.cpp
+++ templates/strings/suffixArray.cpp
@@ -1,27 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-template<typename T> void inputlst(T & lst, int x, int y)
-    { for(int i = x ; i < y; i++ ) cin >> lst[i]; }
-template<typename T> void printlst(T & lst, int x, int y)
-    { for(int i = x ; i < y; i++ ) cout << lst[i] << " "[i > y - 1]; }
-vector<int> sort_cyclic_shifts(string const & s)
-{
+vector<int> suffix_array_construction(string s) {
+    s += "$";
     int n = s.size(), alphabet = 256, classes = 1;
     vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
     for (int i = 0; i < n; i++) cnt[s[i]]++;
     for (int i = 1; i < alphabet; i++) cnt[i] += cnt[i - 1];
     for (int i = 0; i < n; i++) p[--cnt[s[i]]] = i;
     c[p[0]] = 0;
-    for (int i = 1; i < n; i++)
-    {
+    for (int i = 1; i < n; i++) {
         if (s[p[i]] != s[p[i - 1]]) classes++;
         c[p[i]] = classes - 1;
     }
     vector<int> pn(n), cn(n);
-    for (int h = 0; (1 << h) < n; ++h)
-    {
-        for (int i = 0; i < n; i++)
-        {
+    for (int h = 0; (1 << h) < n; ++h) {
+        for (int i = 0; i < n; i++) {
             pn[i] = p[i] - (1 << h);
             if (pn[i] < 0) pn[i] += n;
         }
@@ -30,8 +23,7 @@ vector<int> sort_cyclic_shifts(string const & s)
         for (int i = 1; i < classes; i++) cnt[i] += cnt[i - 1];
         for (int i = n - 1; i >= 0; i--) p[--cnt[c[pn[i]]]] = pn[i];
         cn[p[0]] = 0, classes = 1;
-        for (int i = 1; i < n; i++)
-        {
+        for (int i = 1; i < n; i++) {
             pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
             pair<int, int> prev = {c[p[i - 1]], c[(p[i - 1] + (1 << h)) % n]};
             if (cur != prev) ++classes;
@@ -39,13 +31,8 @@ vector<int> sort_cyclic_shifts(string const & s)
         }
         c.swap(cn);
     }
+    p.erase(p.begin());
     return p;
-}
-vector<int> suffix_array_construction(string s) {
-    s += "$";
-    vector<int> sorted_shifts = sort_cyclic_shifts(s);
-    sorted_shifts.erase(sorted_shifts.begin());
-    return sorted_shifts;
 }
 vector<int> lcp_construction(string const& s, vector<int> const& suffix_array) {
     int n = s.size(), k = 0;
@@ -64,13 +51,12 @@ vector<int> lcp_construction(string const& s, vector<int> const& suffix_array) {
     }
     return lcp;
 }
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     string str1 = "banana";
     vector<int> lst = suffix_array_construction(str1);
-    vector<int> temp = lcp_construction(str1, lst);
-    printlst(lst, 0, lst.size());
+    for (int i = 0; i < lst.size(); i++) cout << lst[i] << " ";
     cout << endl;
-    printlst(temp, 0, temp.size());
+    vector<int> temp = lcp_construction(str1, lst);
+    for (int i = 0; i < temp.size(); i++) cout << temp[i] << " ";
     return 0;
 }
