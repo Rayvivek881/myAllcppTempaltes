f882f36cdba991b881d18bb652cd51cd9caef398
vivek kumar ray <56621398+Rayvivek881@users.noreply.github.com>
2021-09-19 12:49:01 -0700

    Update FenwickTree.cpp

--- templates/Datastructure/FenwickTree.cpp
+++ templates/Datastructure/FenwickTree.cpp
@@ -1,27 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
 template<typename T>
-class FenwickTree{
+class FenwickTree {
 public:
-    int n;
-    T *Tree;
+    int n; vector<int> Tree;
     FenwickTree(int size) : n(size + 2) {
-        Tree = new T[size + 2];
-        for (int i = 0; i < size + 2; i++)
-            Tree[i] = 0;
+        Tree.assign(size + 2, 0);
     }
-    void updateUtil(int ind, int delta){
+    void update(int ind, int delta) {
         for (++ind; ind < n; ind += ind & -ind)
             Tree[ind] += delta;
     }
-    T queryUtil(int ind) {
-        T ans = 0;
+    T queryUtil(int ind, T ans = 0) {
         for (++ind; ind > 0; ind -= ind & -ind)
             ans += Tree[ind];
         return ans;
     }
-    void update(int i, int val){
-        updateUtil(i, val); 
-    }
-    T query(int l, int r){
-        return (queryUtil(r) - queryUtil(l-1));
+    T query(int l, int r) {
+        return queryUtil(r) - queryUtil(l-1);
     }
 };
