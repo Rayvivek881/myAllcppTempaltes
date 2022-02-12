a2abfb9143bade77ee97c5fac419dfb34b5a4b9d
vivek kumar ray <56621398+Rayvivek881@users.noreply.github.com>
2022-02-11 20:06:25 -0800

    Update unionFind.cpp

--- templates/graphs/unionFind.cpp
+++ templates/graphs/unionFind.cpp
@@ -1,24 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-template <typename mydata>
+template <typename T>
 class UnionFind {
 public:
     int Components;
-    vector<mydata> parent, rank;
+    vector<T> parent, rank;
     UnionFind(int size) : Components(size) {
         parent.resize(size);
         rank.assign(size, 0);
         for (int i = 0; i < size; i++)
             parent[i] = i;
     }
-    mydata find(mydata x) {
+    T find(T x) {
         if (parent[x] != x)
             parent[x] = find(parent[x]);
         return parent[x];
     }
     int size() { return Components; }
-    void Union(mydata a, mydata b) {
-        mydata aset = find(a), bset = find(b);
+    void Union(T a, T b) {
+        T aset = find(a), bset = find(b);
         if (aset == bset) return ;
         Components--;
         if (rank[aset] < rank[bset])
