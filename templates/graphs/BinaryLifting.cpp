ddadfc770adac3805c0a54aa2a210dd2f78db57b
vivek kumar ray <56621398+Rayvivek881@users.noreply.github.com>
2022-07-11 22:17:55 -0700

    Update BinaryLifting.cpp

--- templates/graphs/BinaryLifting.cpp
+++ templates/graphs/BinaryLifting.cpp
@@ -1,66 +1,43 @@
-#include <bits/stdc++.h>
-using namespace std;
-template <typename data>
+vector<int> Graph[200005];
+int n, *depth;
 class BinaryLifting {
-public:
-    int n, Log;
-    vector<data> *Graph, *Table;
-    data *lavel;
-    BinaryLifting(int size, vector<data> Tree[]) : n(size) {
-        lavel = new data[size + 1];
-        Graph = new vector<data>[size + 1];
-        Table = new vector<data>[size + 1];
-        Log = ceil(log2(size)) + 1;
-        for (int i = 0; i < size; i++) {
-            Table[i].resize(Log);
-            Graph[i] = Tree[i];
-            lavel[i] = 0;
-        }
-        PreDFS(0, 0);
-    }
-    void PreDFS(data node, data par) {
+    vector<vector<int>> Table;
+    int Log;
+    void answer(int node, int par = -1) {
         Table[node][0] = par;
-        for (int i = 1; i < Log; i++)
-            Table[node][i] = Table[Table[node][i - 1]][i - 1];
-        for (auto child : Graph[node]) {
+        for (int i = 1; i < Log; i++) {
+            if (depth[node] >= (1 << i))
+                Table[node][i] = Table[Table[node][i - 1]][i - 1];
+        }
+        for (auto & child : Graph[node]) {
             if (child == par) continue;
-            lavel[child] = lavel[node] + 1;
-            PreDFS(child, node);
+            depth[child] = depth[node] + 1;
+            answer(child, node);
         }
     }
-    data LCA(data u, data v) {
-        if (lavel[u] < lavel[v]) swap(u, v);
-        for (int i = Log - 1; i >= 0; i--) {
-            if (lavel[u] - pow(2, i) >= lavel[v])
-                u = Table[u][i];
-        }
-        if (u == v) return u;
-        for (int i = Log - 1; i >= 0; i--) {
-            if (Table[u][i] != Table[v][i])
-                u = Table[u][i], v = Table[v][i];
-        }
-        return Table[u][0];
+public:
+    BinaryLifting() {
+        Log = ceil(log2(n)) + 2;
+        Table.assign(n + 1, vector<int>(Log));
+        depth = new int [n + 1], depth[1] = 0;
+        answer(1, -1);
     }
-    data kthAnsester(data node, int k) {
-        for (int i = 0; i < Log; i++) {
-            if (k & (1 << i)) {
-                if ((1 << i) > lavel[node]) return -1;
-                node = Table[node][i];
-            }
-        }
+    int kthAnsester(int node, int k) {
+        if (depth[node] < k) return -1;
+        for (int i = Log - 1; i >= 0; i--)
+            if (k & (1 << i)) node = Table[node][i];
         return node;
     }
-};
-int main(int argc, char const *argv[]) {
-    int n; cin >> n;
-    vector<int> Tree[n];
-    for (int i = 0; i < n - 1; i++) {
-        int u, v;
-        cin >> u >> v;
-        Tree[u - 1].push_back(v - 1);
-        Tree[v - 1].push_back(u - 1);
+    int FindLca(int a, int b) {
+        if (depth[a] >= depth[b]) swap(a, b);
+        int k = abs(depth[a] - depth[b]);
+        for (int i = Log - 1; i >= 0; i--)
+            if (k & (1 << i)) b = Table[b][i];
+        if (a == b) return a;
+        for (int i = Log - 1; i >= 0; i--) {
+            if (Table[a][i] != Table[b][i])
+                a = Table[a][i], b = Table[b][i];
+        }
+        return Table[a][0];
     }
-    BinaryLifting<int> obj(n, Tree);
-    cout << (obj.kthAnsester(5, 1));
-    return 0; 
-}
+};
