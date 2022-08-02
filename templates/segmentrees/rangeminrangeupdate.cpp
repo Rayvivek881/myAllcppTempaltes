84da860eb0d39b4a7b44a253d0c223050a23485d
vivek kumar ray <56621398+Rayvivek881@users.noreply.github.com>
2022-08-02 00:50:14 -0700

    Update rangeminrangeupdate.cpp

--- templates/segmentrees/rangeminrangeupdate.cpp
+++ templates/segmentrees/rangeminrangeupdate.cpp
@@ -1,20 +1,9 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define boost ios_base::sync_with_stdio(false); cin.tie(nullptr);
 template <typename T, class func = function<T(const T &, const T &)>>
-class rangeTree
-{
-public:
-    T *Tree, *lazy;
+class rangeTree {
+    T *Tree, *lazy; 
     int n;
     func myfunc;
     vector<T> lst;
-    rangeTree(int size, vector<T> & arr, const func & F) : lst(arr), n(size), myfunc(F) {
-        Tree = new T[4 * size];
-        lazy = new T[4 * size];
-        BuildTree(1, 0, n - 1);
-    }
     void BuildTree(int ind, int l, int r) {
         if (l == r) {
             Tree[ind] = lst[l], lazy[ind] = 0;
@@ -68,6 +57,12 @@ public:
             return myfunc(a, b);
         }
     }
+public:
+    rangeTree(int size, vector<T> & arr, const func & F) : lst(arr), n(size), myfunc(F) {
+        Tree = new T[4 * size];
+        lazy = new T[4 * size];
+        BuildTree(1, 0, n - 1);
+    }
     void update(int l, int r, T val) {
         updateUtil(1, 0, n - 1, l, r, val);
         return ;
@@ -77,23 +72,3 @@ public:
         return temp;
     }
 };
-int main(int argc, char const *argv[]) {
-    boost;
-    ll n, q; cin >> n >> q;
-    vector<ll> lst(n);
-    for (int i = 0; i < n; i++) cin >> lst[i];
-    rangeTree<ll> segmentTree(n, lst, [&](ll a, ll b) {return min(a, b); });
-    while (q--) {
-        ll flage, l, r, k; cin >> flage;
-        if (flage == 1) {
-            cin >> l >> r >> k;
-            l--, r--;
-            segmentTree.update(l, r, k);
-        }
-        else {
-            cin >> k;
-            cout << segmentTree.query(k - 1, k - 1) << "\n";
-        }
-    }
-    return 0;
-}
